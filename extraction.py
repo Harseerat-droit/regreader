01e9c0eac6b6c6e8f587915b7c18d46f6422b6e0
Harseerat Khaira <harseeratkhaira@ip-192-168-1-44.ec2.internal>
2019-09-20 15:21:34 -0400

    initial commit

---
+++ extraction.py
@@ -0,0 +1,16 @@
+import PyPDF2 as pdf 
+
+def extract_text (filename):
+    regulation = open(filename,'rb')
+
+    regReader = pdf.PdfFileReader(regulation)
+
+    pages_num = regReader.numPages
+    page_text =[]
+
+    for page_num in range (0,pages_num):
+        page=regReader.getPage(page_num)
+        page_text.append(page.extractText)
+
+    regulation.close()
+    return page_text
---
+++ main.py
@@ -0,0 +1,5 @@
+import extraction as e
+
+
+text_of_pages = e.extract_text('secReg.pdf')
+
